8db44dd8c6424c532994099c5c331e31ad0a59a8
Mateus Vinicius da Silva <36384659+MateusVS@users.noreply.github.com>
2019-07-05 10:43:50 -0300

    Implantação empate

--- js/act.js
+++ js/act.js
@@ -52,40 +52,46 @@ function marcar(casa) {
 }
 
 function verifica() {
-    if ((casa1.value == casa2.value && casa2.value == casa3.value && casa1.value == casa3.value) ||
-       (casa4.value == casa5.value && casa5.value == casa6.value && casa4.value == casa6.value) ||
-       (casa7.value == casa8.value && casa8.value == casa9.value && casa7.value == casa9.value) ||
-       (casa1.value == casa4.value && casa4.value == casa7.value && casa7.value == casa1.value) ||
-       (casa2.value == casa5.value && casa5.value == casa8.value && casa8.value == casa2.value) ||
-       (casa3.value == casa6.value && casa6.value == casa9.value && casa9.value == casa3.value) ||
-       (casa1.value == casa5.value && casa5.value == casa9.value && casa1.value == casa9.value) ||
-       (casa3.value == casa5.value && casa3.value == casa7.value && casa7.value == casa5.value)){
-        //alert(casa1.value + casa2.value + casa3.value)
-        if ((casa1.value == 1 && casa2.value == 1 && casa3.value == 1) || 
-            (casa4.value == 1 && casa5.value == 1 && casa6.value == 1) ||
-            (casa7.value == 1 && casa8.value == 1 && casa9.value == 1) || 
-            (casa1.value == 1 && casa4.value == 1 && casa7.value == 1) ||
-            (casa2.value == 1 && casa5.value == 1 && casa8.value == 1) || 
-            (casa3.value == 1 && casa6.value == 1 && casa9.value == 1) ||
-            (casa1.value == 1 && casa5.value == 1 && casa9.value == 1) ||
-            (casa3.value == 1 && casa5.value == 1 && casa7.value == 1)){
-                alert("Fim de jogo !!!! O vencedor foi " + j1);
-                pt1++;
-                document.getElementById("pt1").innerHTML = pt1;
-                reseta();
-        }if ((casa1.value == 2 && casa2.value == 2 && casa3.value == 2) || 
-            (casa4.value == 2 && casa5.value == 2 && casa6.value == 2) ||
-            (casa7.value == 2 && casa8.value == 2 && casa9.value == 2) || 
-            (casa1.value == 2 && casa4.value == 2 && casa7.value == 2) ||
-            (casa2.value == 2 && casa5.value == 2 && casa8.value == 2) || 
-            (casa3.value == 2 && casa6.value == 2 && casa9.value == 2) ||
-            (casa1.value == 2 && casa5.value == 2 && casa9.value == 2) ||
-            (casa3.value == 2 && casa5.value == 2 && casa7.value == 2)){
-                alert("Fim de jogo !!!! O vencedor foi " + j2);
-                pt2++;
-                document.getElementById("pt2").innerHTML = pt2;
-                reseta();
+    if (casa1.value != 0 && casa2.value != 0 && casa3.value != 0 && casa4.value !=0 && casa5.value !=0 && casa6.value !=0 && casa7.value !=0 && casa8.value !=0 && casa9.value != 0) {
+        if ((casa1.value == casa2.value && casa2.value == casa3.value && casa1.value == casa3.value) ||
+            (casa4.value == casa5.value && casa5.value == casa6.value && casa4.value == casa6.value) ||
+            (casa7.value == casa8.value && casa8.value == casa9.value && casa7.value == casa9.value) ||
+            (casa1.value == casa4.value && casa4.value == casa7.value && casa7.value == casa1.value) ||
+            (casa2.value == casa5.value && casa5.value == casa8.value && casa8.value == casa2.value) ||
+            (casa3.value == casa6.value && casa6.value == casa9.value && casa9.value == casa3.value) ||
+            (casa1.value == casa5.value && casa5.value == casa9.value && casa1.value == casa9.value) ||
+            (casa3.value == casa5.value && casa3.value == casa7.value && casa7.value == casa5.value)){
+            //alert(casa1.value + casa2.value + casa3.value)
+            if ((casa1.value == 1 && casa2.value == 1 && casa3.value == 1) || 
+                (casa4.value == 1 && casa5.value == 1 && casa6.value == 1) ||
+                (casa7.value == 1 && casa8.value == 1 && casa9.value == 1) || 
+                (casa1.value == 1 && casa4.value == 1 && casa7.value == 1) ||
+                (casa2.value == 1 && casa5.value == 1 && casa8.value == 1) || 
+                (casa3.value == 1 && casa6.value == 1 && casa9.value == 1) ||
+                (casa1.value == 1 && casa5.value == 1 && casa9.value == 1) ||
+                (casa3.value == 1 && casa5.value == 1 && casa7.value == 1)){
+                    alert("Fim de jogo !!!! O vencedor foi " + j1);
+                    pt1++;
+                    document.getElementById("pt1").innerHTML = pt1;
+                    reseta();
+            }else if ((casa1.value == 2 && casa2.value == 2 && casa3.value == 2) || 
+                (casa4.value == 2 && casa5.value == 2 && casa6.value == 2) ||
+                (casa7.value == 2 && casa8.value == 2 && casa9.value == 2) || 
+                (casa1.value == 2 && casa4.value == 2 && casa7.value == 2) ||
+                (casa2.value == 2 && casa5.value == 2 && casa8.value == 2) || 
+                (casa3.value == 2 && casa6.value == 2 && casa9.value == 2) ||
+                (casa1.value == 2 && casa5.value == 2 && casa9.value == 2) ||
+                (casa3.value == 2 && casa5.value == 2 && casa7.value == 2)){
+                    alert("Fim de jogo !!!! O vencedor foi " + j2);
+                    pt2++;
+                    document.getElementById("pt2").innerHTML = pt2;
+                    reseta();
+            }
+        }else{
+            alert("Partida terminou empatada");
+            reseta()
         }
+
     }
 }
 
